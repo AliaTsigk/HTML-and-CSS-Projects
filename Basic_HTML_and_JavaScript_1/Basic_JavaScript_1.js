86bac24eec7f5bb6b1d355bc8e393a22229a932f
AliaTsigk <alia7843@hotmail.gr>
2023-12-03 17:44:53 +0000

    Website Practice
    This is for learning purposes only

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,8 +1,2 @@
 alert("Hello World! I am an alert box!");
 
-function My_first_function() {
-    var string = "Kiss me, I'm Irish!";
-	var result = string.fontcolor("green");
-	document.getElementById("Irish").innerHTML = 
-	result;
-  }
